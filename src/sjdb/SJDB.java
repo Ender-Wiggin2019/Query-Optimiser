3217f12cb783fb11f9e3bddf9055527ce8f89b47
Oushuo Huang <49976407+Ender-Wiggin2019@users.noreply.github.com>
2023-04-21 02:48:27 +0100

    clean code

--- src/sjdb/SJDB.java
+++ src/sjdb/SJDB.java
@@ -15,25 +15,16 @@ public class SJDB {
 	 */
 	public static void main(String[] args) throws Exception {
 		// read serialised catalogue from file and parse
-		// TODO: find a better solution
-		String catFile = "data/cat.txt";
-//		String catFile = args[0];
+
+		String catFile = args[0];
 		Catalogue cat = new Catalogue();
 		CatalogueParser catParser = new CatalogueParser(catFile, cat);
 		catParser.parse();
 
 		System.out.println(catParser);
 		// read stdin, parse, and build canonical query plan
-//		QueryParser queryParser = new QueryParser(cat, new InputStreamReader(System.in));
-
-		// TODO: for test
-		QueryParser queryParser = new QueryParser(cat, new FileReader(new File("data/q5.txt")));
-
+		QueryParser queryParser = new QueryParser(cat, new InputStreamReader(System.in));
 		Operator plan = queryParser.parse();
-
-		System.out.println("-----------------------------  PLAN  -------------------------------------------------------------");
-
-		System.out.println(plan.toString());
 				
 		// create estimator visitor and apply it to canonical plan
 		Estimator est = new Estimator();
